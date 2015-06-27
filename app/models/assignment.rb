30b48c4cbd2122675a6bb0ae64609c25746b3d11
Jesse Shawl <jesse@jshawl.com>
2015-06-27 12:59:22 -0400

    seed submissions on assignment creation

--- app/models/assignment.rb
+++ app/models/assignment.rb
@@ -1,3 +1,13 @@
 class Assignment < ActiveRecord::Base
   has_many :submissions
+  after_create :seed_submissions
+  private
+  def seed_submissions
+    students = Student.all
+    students.each do |student|
+      if student["name"]
+	self.submissions.create( github_id: student["github_user_id"] )
+      end
+    end
+  end
 end
--- app/models/submission.rb
+++ app/models/submission.rb
@@ -1,6 +1,11 @@
 class Submission < ActiveRecord::Base
   belongs_to :assignment, dependent: :destroy
   def student
-    JSON.parse(HTTParty.get("http://api.wdidc.org/students/#{self.github_id}").body)
+    res = JSON.parse(HTTParty.get("http://api.wdidc.org/students/#{self.github_id}").body)
+    if res.class == Hash
+      return res
+    else
+      return {name:""}
+    end
   end
 end
