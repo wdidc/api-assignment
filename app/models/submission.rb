d585c9b54d58fe70fb5a8f8e9ff9d116d5cb6959
RobertAKARobin <robertgfthomas@gmail.com>
2015-06-30 13:09:55 -0400

    Show all issues for that squad

--- app/models/submission.rb
+++ app/models/submission.rb
@@ -1,6 +1,11 @@
 class Submission < ActiveRecord::Base
   belongs_to :assignment
   @@students = JSON.parse(HTTParty.get("http://api.wdidc.org/students").body)
+
+  def students
+    @@students
+  end
+
   def student
     @@students.each do |student|
       if student["github_user_id"] == self.github_id
@@ -8,7 +13,20 @@ class Submission < ActiveRecord::Base
       end
     end
   end
+
   def as_json(options={})
     super.as_json(options).merge({assignment_title: self.assignment.title})
   end
+
+  def involves_squad
+    studs = @@students.select{|s|
+      s["squad"] === student["squad"]
+    }
+    query = []
+    studs.each{|s|
+      query.push("involves%3A#{s["github_username"]}")
+    }
+    return query.join("+")
+  end
+
 end
