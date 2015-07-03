a4f23703b5296411b6690f098584e9f6d8666a83
Jesse Shawl <jesse@jshawl.com>
2015-07-03 19:25:34 -0400

    remove binding

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -27,7 +27,6 @@ class ApplicationController < ActionController::Base
   def authenticate_user!
     errors = []
     errors << {error:"Not an instructor"} unless is_an_instructor?
-    binding.pry
     errors << {error:"Missing valid api token"} unless has_api_token?
     render json: errors unless errors.empty?
   end
