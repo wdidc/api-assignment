ac016461ac15fa2df67e51499f1bde3cbb594047
mattscilipoti <matt.scilipoti@ga.co>
2015-07-08 14:09:19 -0400

    Fix #8 Link to StudentTracker (if instructor)
    Added student_url helper
    Now logs error, instead of swallowing it.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,4 +3,9 @@ module ApplicationHelper
   def repo_name url
     return url[(url.rindex("/") + 1)..-1] if url.include? "/"
   end
+
+  def student_url(student_json)
+    github_id = student_json.fetch('github_user_id')
+    URI::HTTP.build(host: 'students.wdidc.org', path: "/#{github_id}").to_s
+  end
 end
