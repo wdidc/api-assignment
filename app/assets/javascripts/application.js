7db9b817bbd5421329ed26149fc0f1ece515211d
Jesse Shawl <jesse@jshawl.com>
2015-07-19 14:59:50 -0400

    wip autosave

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,6 +14,23 @@
 //= require jquery_ujs
 //= require_tree .
 
+$.fn.serializeObject = function()
+{
+    var o = {};
+    var a = this.serializeArray();
+    $.each(a, function() {
+        if (o[this.name] !== undefined) {
+            if (!o[this.name].push) {
+                o[this.name] = [o[this.name]];
+            }
+            o[this.name].push(this.value || '');
+        } else {
+            o[this.name] = this.value || '';
+        }
+    });
+    return o;
+};
+
 $(function(){
   $(".status input").change(function(){
     $.ajax({
@@ -31,4 +48,26 @@ $(function(){
       console.dir(response);
     });
   });
+
+
+  $("[autosave]").on("keyup", isTyping)
+  var typingTimer
+  var isTyping = function isTyping( event ){
+      clearTimeout(typingTimer)
+      typingTimer = setTimeout(doneTyping, 5000)
+  }
+  var doneTyping = function doneTyping(){
+    var $form = $(this).find("form")
+    $.ajax({
+      url: $form.attr("action"),
+      type: "post",
+      dataType: "json",
+      data: {
+	_method: "patch",
+	submission: $form.serializeObject()
+      }
+    }).always(function(response){
+      console.dir(response);
+    });
+  }
 })
